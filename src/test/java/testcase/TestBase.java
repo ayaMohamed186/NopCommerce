776606f46d79600f0c24db55ea864d8419afedae
ayaMohamed186 <37931347+ayaMohamed186@users.noreply.github.com>
2024-01-03 01:14:14 +0200

    allure report update

--- src/test/java/testcase/TestBase.java
+++ src/test/java/testcase/TestBase.java
@@ -1,5 +1,9 @@
 package testcase;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.github.javafaker.Faker;
 import common.MyScreenRecorder;
 import drivers.DriverFactory;
@@ -9,6 +13,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import pages.P01_HomePage;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static drivers.DriverHolder.setDriver;
@@ -16,27 +23,66 @@ import static drivers.DriverHolder.setDriver;
 public class TestBase {
 
     WebDriver driver;
-    static Faker faker=new Faker();
+    static Faker faker = new Faker();
+    protected static ExtentSparkReporter htmlReporter;
+    protected static ExtentReports extent;
+    protected static ExtentTest test;
+    private static String PROJECT_NAME = null;
+    private static String PROJECT_URL = null;
+    static Properties prop;
+    static FileInputStream readProperty;
+
 
     @BeforeSuite
     public void defineSuite() throws Exception {
         MyScreenRecorder.startRecording("E2E Nop-Commerce");
+
+        // initialize the HtmlReporter
+        htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/testReport.html");
+
+        //initialize ExtentReports and attach the HtmlReporter
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        setProjectDetails();
+
+        // initialize test
+        test = extent.createTest(PROJECT_NAME + " Test Automation Project");
+
+        //configuration items to change the look and fee add content, manage tests etc
+        htmlReporter.config().setDocumentTitle(PROJECT_NAME + " Test Automation Report");
+        htmlReporter.config().setReportName(PROJECT_NAME + " Test Report");
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+    }
+
+    private void setProjectDetails() throws IOException, IOException {
+        // TODO: Step1: define object of properties file
+        readProperty = new FileInputStream(
+                System.getProperty("user.dir") + "/src/test/resources/properties/environment.properties");
+        prop = new Properties();
+        prop.load(readProperty);
+
+        // define project name from properties file
+        PROJECT_NAME = prop.getProperty("projectName");
+        PROJECT_URL = prop.getProperty("url");
     }
 
     @Parameters("browser")
 
     @BeforeTest
-    public void setupDriver(String browser){
+    public void setupDriver(String browser) {
         driver = DriverFactory.getNewInstance(browser);
         setDriver(driver);
-        driver.get("https://demo.nopcommerce.com/");
+        driver.get(PROJECT_URL);
     }
 
     @AfterTest
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
         Thread.currentThread().interrupt(); // after driver close , close opened thread which open in each run
     }
+
     @AfterSuite
     public void endSuite() throws Exception {
         MyScreenRecorder.stopRecording();
